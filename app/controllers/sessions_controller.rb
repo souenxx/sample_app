4fd197263a28af5f62d9a944ee3f740456025604
souen <souen_xx@yahoo.co.jp>
2019-11-21 07:34:02 +0000

    Implement basic login

---
+++ app/controllers/sessions_controller.rb
@@ -0,0 +1,22 @@
+class SessionsController < ApplicationController
+  def new
+  end
+  
+  def create
+    user=User.find_by(email: params[:session][:email].downcase)
+    if user && user.authenticate(params[:session][:password]) 
+      log_in user
+      redirect_to user
+      #ユーザ情報のページにリダイレクト
+    else
+      #エラーメッセージ作成
+      flash.now[:danger]='Invalid email/password combination' 
+      render 'new'
+    end
+  end
+  
+  def destroy
+    log_out
+    redirect_to root_url
+  end
+end
