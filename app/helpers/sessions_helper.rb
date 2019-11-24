325980c9394719193a5c71f17c5d64c94abf4873
souen <souen_xx@yahoo.co.jp>
2019-11-24 02:21:43 +0000

    Finish user edit, update, index, and destroy actions

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -11,6 +11,10 @@ module SessionsHelper
     cookies.permanent[:remember_token] = user.remember_token
   end
   
+  def current_user?(user)
+    user==current_user
+  end
+  
   def current_user
     if (user_id=session[:user_id])
       @current_user ||= User.find_by(id: session[:user_id])
@@ -37,5 +41,16 @@ module SessionsHelper
     user.forget
     cookies.delete(:user_id)
     cookies.delete(:remember_token)
+  end
+  
+  #記憶したURLにリダイレクト
+  def redirect_back_or(default)
+    redirect_to(session[:forwarding_url] || default)
+    session.delete(:forwarding_url)
+  end
+  
+  #アクセスしようとしたURLを記憶
+  def store_location
+    session[:forwarding_url] = request.original_url if request.get?
   end
 end
